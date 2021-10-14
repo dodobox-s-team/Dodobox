31fdd0def30e0638a8131f22480759300c68985b
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2021-10-14 16:20:15 +0200

    [new add]route details
    Permet de pouvoir afficher la page details

---
+++ web/src/pages/Details.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Details = () => {
+	return (
+		<h1>d2tqils</h1>
+	);
+};
+
+export default Details;
