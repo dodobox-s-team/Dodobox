93087b4b267230550649b6101061f3e44a1d40f9
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2021-10-18 10:56:03 +0200

    [modif]ajout list
    Liste des données des différents appareils

--- web/src/pages/Details.tsx
+++ web/src/pages/Details.tsx
@@ -1,11 +1,52 @@
 import { useState } from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge, MDBSwitch } from "mdbreact";
+import { Component } from "react";
 
 const Details = () => {
-	return (
-		<h1>d2tqils</h1>
-	);
+    return (
+        <div>
+        <MDBContainer>
+        <MDBListGroup style={{ width: "22rem" }}>
+          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Name: {'exemple'}<MDBBadge color="primary"
+              pill>14</MDBBadge>
+          </MDBListGroupItem>
+          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Groupe: {'exemple'}<MDBBadge color="primary"
+              pill>1</MDBBadge>
+          </MDBListGroupItem>
+          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Modèle: {'exemple'}<MDBBadge
+              color="primary" pill>2</MDBBadge>
+          </MDBListGroupItem>
+          <MDBListGroupItem className="d-flex justify-content-between align-items-center">IP: {'exemple'}<MDBBadge color="primary"
+              pill>1</MDBBadge>
+          </MDBListGroupItem>
+          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Type: {'exemple'}<MDBBadge color="primary"
+              pill>1</MDBBadge>
+          </MDBListGroupItem>
+        </MDBListGroup>
+      </MDBContainer>
+      </div>
+      <div>
+      state = {
+        switch1: true,
+      }
+      
+      handleSwitchChange = nr => () => {
+        let switchNumber = `switch${nr}`;
+        this.setState({
+          [switchNumber]: !this.state[switchNumber]
+        });
+      }
+      
+      render() {
+        return (
+          <MDBSwitch checked={this.state.switch1} onChange={this.handleSwitchChange(1)} />
+          );
+        }
+      }
+      </div>
+    );
 };
 
 export default Details;
