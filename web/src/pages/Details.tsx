bea2e797e00f999a23de8a32ccab740c41c404d4
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2021-10-20 11:13:38 +0200

    [fix bug][modif]ajout switch

--- web/src/pages/Details.tsx
+++ web/src/pages/Details.tsx
@@ -1,52 +1,41 @@
-import { useState } from 'react'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge, MDBSwitch } from "mdbreact";
-import { Component } from "react";
+import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge } from "mdbreact";
+import { Form } from 'react-bootstrap'
 
 const Details = () => {
-    return (
-        <div>
-        <MDBContainer>
-        <MDBListGroup style={{ width: "22rem" }}>
-          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Name: {'exemple'}<MDBBadge color="primary"
-              pill>14</MDBBadge>
-          </MDBListGroupItem>
-          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Groupe: {'exemple'}<MDBBadge color="primary"
-              pill>1</MDBBadge>
-          </MDBListGroupItem>
-          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Modèle: {'exemple'}<MDBBadge
-              color="primary" pill>2</MDBBadge>
-          </MDBListGroupItem>
-          <MDBListGroupItem className="d-flex justify-content-between align-items-center">IP: {'exemple'}<MDBBadge color="primary"
-              pill>1</MDBBadge>
-          </MDBListGroupItem>
-          <MDBListGroupItem className="d-flex justify-content-between align-items-center">Type: {'exemple'}<MDBBadge color="primary"
-              pill>1</MDBBadge>
-          </MDBListGroupItem>
-        </MDBListGroup>
-      </MDBContainer>
-      </div>
+  return (
+    <>
       <div>
-      state = {
-        switch1: true,
-      }
-      
-      handleSwitchChange = nr => () => {
-        let switchNumber = `switch${nr}`;
-        this.setState({
-          [switchNumber]: !this.state[switchNumber]
-        });
-      }
-      
-      render() {
-        return (
-          <MDBSwitch checked={this.state.switch1} onChange={this.handleSwitchChange(1)} />
-          );
-        }
-      }
+        <MDBContainer>
+          <MDBListGroup style={{ width: "22rem" }}>
+            <MDBListGroupItem className="d-flex justify-content-between align-items-center">Name: {'exemple'}<MDBBadge color="primary"
+              pill>14</MDBBadge>
+            </MDBListGroupItem>
+            <MDBListGroupItem className="d-flex justify-content-between align-items-center">Groupe: {'exemple'}<MDBBadge color="primary"
+              pill>1</MDBBadge>
+            </MDBListGroupItem>
+            <MDBListGroupItem className="d-flex justify-content-between align-items-center">Modèle: {'exemple'}<MDBBadge
+              color="primary" pill>2</MDBBadge>
+            </MDBListGroupItem>
+            <MDBListGroupItem className="d-flex justify-content-between align-items-center">IP: {'exemple'}<MDBBadge color="primary"
+              pill>1</MDBBadge>
+            </MDBListGroupItem>
+            <MDBListGroupItem className="d-flex justify-content-between align-items-center">Type: {'exemple'}<MDBBadge color="primary"
+              pill>1</MDBBadge>
+            </MDBListGroupItem>
+          </MDBListGroup>
+        </MDBContainer>
       </div>
-    );
+
+      <div>
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          label="système online"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Details;
