5be441243474b9b79ed7c43d7e4504597df9891c
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-11-08 18:25:38 +0100

    Insert Graphique in detail

--- web/src/pages/Details.tsx
+++ web/src/pages/Details.tsx
@@ -1,81 +1,93 @@
 import React from 'react'
-import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge } from "mdbreact";
+import {MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge} from "mdbreact";
+import GraphiqueEnergie from "../components/GraphiqueEnergie";
 //import { Form } from 'react-bootstrap'
 import {useParams} from 'react-router-dom'
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      devices: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            devices: []
+        };
+    }
 
-  detailsApi(idParameter) {
+    detailsApi(idParameter) {
 
-    fetch(`https://localhost/api/devices/` + idParameter)
-      .then(res => res.json())
-      .then((result) => {
-        this.setState({
-          isLoaded: true,
-          devices: result
-        })
-      },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+        fetch(`https://localhost/api/devices/` + idParameter)
+            .then(res => res.json())
+            .then((result) => {
+                    this.setState({
+                        isLoaded: true,
+                        devices: result
+                    })
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+
+    render() {
+        const {error, isLoaded, devices} = this.state;
+        console.log(devices);
+        let id = this.props.match.params.id
+        console.log(id);
+        console.log(typeof (id));
+
+
+        window.onload = () => this.detailsApi(id);
+
+        if (error) {
+            return <div>Erreur: {error.message}</div>
+        } else if (!isLoaded) {
+            return <div>Chargement...</div>;
+        } else {
+            return (
+                <div>
+
+                    <MDBContainer>
+                        <MDBListGroup style={{width: "22rem"}}>
+                            <MDBListGroupItem
+                                className="d-flex justify-content-between align-items-center">Name: {devices.name}<MDBBadge
+                                color="primary"
+                                pill>14</MDBBadge>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem
+                                className="d-flex justify-content-between align-items-center">Groupe: {devices.groupId}<MDBBadge
+                                color="primary"
+                                pill>1</MDBBadge>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem
+                                className="d-flex justify-content-between align-items-center">Modèle: {devices.modele}<MDBBadge
+                                color="primary" pill>2</MDBBadge>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem
+                                className="d-flex justify-content-between align-items-center">IP: {devices.ip}<MDBBadge
+                                color="primary"
+                                pill>1</MDBBadge>
+                            </MDBListGroupItem>
+                            <MDBListGroupItem
+                                className="d-flex justify-content-between align-items-center">Type: {devices.type}<MDBBadge
+                                color="primary"
+                                pill>1</MDBBadge>
+                            </MDBListGroupItem>
+                        </MDBListGroup>
+                        <GraphiqueEnergie data={[15, 24, 18, 19]} labels={["test1", "test2", "test3", "test4"]}/>
+                    </MDBContainer>
+
+
+                </div>
+            )
         }
-      )
-  }
-
-
-  render() {
-    const { error, isLoaded, devices} = this.state;
-    console.log(devices);
-    let id = this.props.match.params.id
-    console.log(id);
-    console.log(typeof(id));
-
-
-    window.onload = () => this.detailsApi(id);
-
-    if (error) {
-      return <div>Erreur: {error.message}</div>
-    } else if (!isLoaded) {
-      return <div>Chargement...</div>;
-    } else {
-      return (
-        <div>
-
-          <MDBContainer>
-            <MDBListGroup style={{ width: "22rem" }}>
-              <MDBListGroupItem className="d-flex justify-content-between align-items-center">Name: {devices.name}<MDBBadge color="primary"
-                pill>14</MDBBadge>
-              </MDBListGroupItem>
-              <MDBListGroupItem className="d-flex justify-content-between align-items-center">Groupe: {devices.groupId}<MDBBadge color="primary"
-                pill>1</MDBBadge>
-              </MDBListGroupItem>
-              <MDBListGroupItem className="d-flex justify-content-between align-items-center">Modèle: {devices.modele}<MDBBadge
-                color="primary" pill>2</MDBBadge>
-              </MDBListGroupItem>
-              <MDBListGroupItem className="d-flex justify-content-between align-items-center">IP: {devices.ip}<MDBBadge color="primary"
-                pill>1</MDBBadge>
-              </MDBListGroupItem>
-              <MDBListGroupItem className="d-flex justify-content-between align-items-center">Type: {devices.type}<MDBBadge color="primary"
-                pill>1</MDBBadge>
-              </MDBListGroupItem>
-            </MDBListGroup>
-          </MDBContainer>
-
-        
-      </div>
-      )
+        ;
     };
-  };
 }
 
 export default Details;
