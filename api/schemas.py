bb4ce27ad573aa0b35df931af8a970060379b7d8
Mathieu Walravens <m.walravens@students.ephec.be>
2021-11-10 12:56:17 +0100

    Set groupId as foreign key

--- api/schemas.py
+++ api/schemas.py
@@ -12,7 +12,7 @@ devices = Table(
     "devices",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("groupId", Integer),
+    Column("groupId", Integer, ForeignKey('groups.id')),
     Column("name", String(64), nullable=False),
     Column("modele", String(64), nullable=False),
     Column("type", Integer, nullable=False),
