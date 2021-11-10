003f678ab86c6d375d02942a17e5396f5670dd9c
Mathieu Walravens <m.walravens@students.ephec.be>
2021-11-10 16:03:27 +0100

    Add group details page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,44 +6,41 @@ import ListDevices from "./pages/ListDevices";
 import Details from "./pages/Details";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GroupDetail from "./pages/GroupDetail";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import PageNotFound from "./pages/errors/404";
 //window.id = 1;
 
 function App() {
-  const styles = {
-    contentDiv: {
-      display: "flex",
-    },
-    contentMargin: {
-      width: "100%",
-    },
-  };
+    const styles = {
+        contentDiv: {
+            display: "flex",
+        },
+        contentMargin: {
+            marginLeft: "10px",
+            width: "100%",
+        },
+    };
 
-  return (
-    <Router>
-      <div style={styles.contentDiv}>
-        <SideBar></SideBar>
-        <div style={styles.contentMargin}>
-          <Switch>
-            <Route path="/ListDevices">
-              <ListDevices />
-            </Route>
-            <Route path="/Details/:id" component={Details}></Route>
-            <Route path="/group/:id" component={GroupDetail}></Route>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route path="/">
-              <PageNotFound />
-            </Route>
-          </Switch>
-        </div>
-        <ToastContainer />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div style={styles.contentDiv}>
+                <SideBar></SideBar>
+                <div style={styles.contentMargin}>
+                    <Switch>
+                        <Route path="/ListDevices">
+                            <ListDevices />
+                        </Route>
+                        <Route path="/Details/:id" component={Details}></Route>
+                        <Route
+                            path="/group/:id"
+                            component={GroupDetail}
+                        ></Route>
+                        <Route path="/">
+                            <Dashboard />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
