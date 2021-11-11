b8f320b49c83a0d6aa7b588a4a9516e1e86b4e94
Mathieu Walravens <m.walravens@students.ephec.be>
2021-11-11 18:03:20 +0100

    Add 404 page

--- web/src/pages/GroupDetail.tsx
+++ web/src/pages/GroupDetail.tsx
@@ -1,48 +1,64 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
+import PageNotFound from "./errors/404";
 
 interface Group {
+  group: {
     id: number;
     name: string;
+  };
+  error?: React.ReactElement;
 }
 
 type GroupProps = RouteComponentProps<{
-    id: string;
+  id: string;
 }>;
 
 class GroupDetail extends React.Component<GroupProps, Group> {
-    state: Group = {
-        id: 0,
-        name: "Loading ...",
-    };
+  state: Group = {
+    group: { id: 0, name: "Loading ..." },
+  };
 
-    get id() {
-        return this.props.match.params.id;
-    }
+  get id() {
+    return this.props.match.params.id;
+  }
 
-    componentDidUpdate(props: GroupProps) {
-        if (props.match.params.id != this.id) this.loadGroup();
-    }
+  componentDidUpdate(props: GroupProps) {
+    if (props.match.params.id != this.id) this.loadGroup();
+  }
 
-    componentDidMount() {
-        this.loadGroup();
-    }
+  componentDidMount() {
+    this.loadGroup();
+  }
 
-    loadGroup() {
-        fetch(`/api/groups/${this.id}`, { method: "GET" })
-            .then((r) => r.json())
-            .then((group) => {
-                this.setState(group);
+  loadGroup() {
+    fetch(`/api/groups/${this.id}`, { method: "GET" }).then(async (r) => {
+      if (!r.ok) {
+        switch (r.status) {
+          case 404:
+            this.setState({
+              error: <PageNotFound message="Ce groupe n'existe pas" />,
             });
+            break;
+        }
+        return;
+      }
+
+      this.setState({ group: await r.json(), error: undefined });
+    });
+  }
+
+  render() {
+    if (this.state.error !== undefined) {
+      return this.state.error;
     }
 
-    render() {
-        return (
-            <div>
-                {this.state.id} {this.state.name}
-            </div>
-        );
-    }
+    return (
+      <div>
+        {this.state.group.id} {this.state.group.name}
+      </div>
+    );
+  }
 }
 
 export default GroupDetail;
