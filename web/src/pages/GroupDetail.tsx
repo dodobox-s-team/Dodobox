1c29f274ae29416b289a9da96815c094a391fe9d
Mathieu Walravens <m.walravens@students.ephec.be>
2021-11-11 01:55:47 +0100

    Fix group page not updating

--- web/src/pages/GroupDetail.tsx
+++ web/src/pages/GroupDetail.tsx
@@ -6,22 +6,30 @@ interface Group {
     name: string;
 }
 
-interface GroupParams {
+type GroupProps = RouteComponentProps<{
     id: string;
-}
+}>;
 
-class GroupDetail extends React.Component<{}, Group> {
-    constructor(props: RouteComponentProps<GroupParams>) {
-        super(props);
-        this.state = {
-            id: parseInt(props.match.params.id),
-            name: "Loading ...",
-        };
+class GroupDetail extends React.Component<GroupProps, Group> {
+    state: Group = {
+        id: 0,
+        name: "Loading ...",
+    };
+
+    get id() {
+        return this.props.match.params.id;
+    }
+
+    componentDidUpdate(props: GroupProps) {
+        if (props.match.params.id != this.id) this.loadGroup();
+    }
+
+    componentDidMount() {
         this.loadGroup();
     }
 
     loadGroup() {
-        fetch("/api/groups/" + this.state.id, { method: "GET" })
+        fetch(`/api/groups/${this.id}`, { method: "GET" })
             .then((r) => r.json())
             .then((group) => {
                 this.setState(group);
