1cf410500357e8bf0d40eed7a38b319e7ffd84b7
Mathieu Walravens <m.walravens@students.ephec.be>
2021-11-12 21:55:27 +0100

    Add modify group's name

--- web/src/pages/GroupDetail.tsx
+++ web/src/pages/GroupDetail.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { FaCog } from "react-icons/fa";
 import { RouteComponentProps } from "react-router";
 import PageNotFound from "./errors/404";
+import "../styles/GroupDetail.scss";
+import { toast } from "react-toastify";
 
 interface Group {
-  group: {
+  name: string;
+  group?: {
     id: number;
     name: string;
   };
@@ -16,7 +21,7 @@ type GroupProps = RouteComponentProps<{
 
 class GroupDetail extends React.Component<GroupProps, Group> {
   state: Group = {
-    group: { id: 0, name: "Loading ..." },
+    name: "Loading ...",
   };
 
   get id() {
@@ -44,18 +49,74 @@ class GroupDetail extends React.Component<GroupProps, Group> {
         return;
       }
 
-      this.setState({ group: await r.json(), error: undefined });
+      const group = await r.json();
+      this.setState({ group: group, name: group.name, error: undefined });
     });
   }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // @ts-ignore
+    e.target.parentNode.dataset.value = e.target.value;
+    this.setState({ name: e.target.value });
+  };
+
+  saveChange = () => {
+    // Check if the name has changed
+    if (
+      !this.state.group ||
+      !this.state.name ||
+      this.state.name.trim() == this.state.group?.name.trim()
+    )
+      return;
+
+    fetch(`/api/groups/${this.state.group.id}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: this.state.name.trim() }),
+    }).then(async (r) => {
+      const result = await r.json();
+
+      if (r.ok) {
+        this.setState({ name: result.name, group: result });
+      } else {
+        // @ts-ignore
+        this.setState({ name: this.state.group.name });
+        toast.error(`Impossible de renommer ce groupe: ${result.detail}`);
+      }
+    });
+  };
 
   render() {
     if (this.state.error !== undefined) {
       return this.state.error;
     }
 
+    const style = {
+      outline: "none",
+    };
     return (
       <div>
-        {this.state.group.id} {this.state.group.name}
+        <Container fluid>
+          <Row className="bg-primary text-white p-3 align-items-center">
+            <Col>
+              <span className="input-container text-white rounded">
+                <input
+                  size={1}
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                  onBlur={this.saveChange}
+                  maxLength={64}
+                />
+              </span>
+            </Col>
+            <Col md="auto">
+              <FaCog size="1.3em" />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
