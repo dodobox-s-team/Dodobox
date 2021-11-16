4ce31b0b6dd49638069ace1e1ef9c4d194431ae0
Quentins <HE201860@students.ephec.be>
2021-11-16 17:01:50 +0100

    add test integration graphData + troubleshooting

--- api/routers/graphData.py
+++ api/routers/graphData.py
@@ -6,6 +6,7 @@ router = APIRouter(
     tags=["graphData"],
 )
 
+
 @router.post("", response_model=GraphData)
 async def add_graphdata(graphdata: GraphData):
     """Add a graphdata."""
@@ -16,3 +17,23 @@ async def add_graphdata(graphdata: GraphData):
 async def get_graphdata():
     """Get a list of time bucketed graphdata."""
     return await GraphData.get_all()
+
+@router.delete("/{id}", response_model=GraphData)
+async def delete_graphData(id: int):
+    """Delete a data from Graph"""
+    graphdata = await GraphData.delete(id)
+    if not graphdata:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No data with that ID was found.")
+    
+    return device
+
+@router.put("/{id}", response_model=GraphData)
+async def edit_a_graphdata(id: int, graphData: GraphData):
+    """Updates informations about data."""
+    updated_graphData = await GraphData.edit(id, graphData)
+    if not updated_graphData:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No data with that ID was found.")
+
+    return updated_graphData 
+
+
