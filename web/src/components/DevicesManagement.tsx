7194adc1244397503e6ced4adbcd0ada2f41ee89
sebastienvolont <he201833@students.ephec.be>
2021-11-24 11:29:55 +0100

    (fix) Required changes for Pull request review #15
    This commit contains correct 2 spaces indentation, types precision, removal of constructors and console.log, absolute path api.

--- web/src/components/DevicesManagement.tsx
+++ web/src/components/DevicesManagement.tsx
@@ -1,121 +1,140 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import FormDeviceOptions from './FormDeviceOptions.tsx'
-import {Modal, Button, Form, Image, Alert} from 'react-bootstrap'
+import { Modal, Button, Form, Image, Alert } from 'react-bootstrap'
 import { RiSettings2Line } from "react-icons/ri";
 
-class DevicesManagement extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      modalShow: false,
-      setModalShow: false,
-      validAlertShow: false,
-      invalidAlertShow: false,
-      setInvalidAlertShow: false,
-      setValidAlertShow: false,
-      messageErreur: "",
-        }
-    };
-
-    handleClose() {
-      this.setState({modalShow: false});
-      this.setState({validAlertShow: false});
-    }
-    handleShow() {
-      this.setState({modalShow: true});
-    }
-    handleValidAlertShow() {
-      if (this.invalidAlertShow) {
-        this.setState({validAlertShow: false});
-    } else {
-        this.setState({validAlertShow: true});
-        this.setState({invalidAlertShow: false});
-        }
-    }
-
-    handleInvalidAlertShow() {
-      if (this.validAlertShow) {
-        this.setState({invalidAlertShow: false})
-    } else {
-        this.setState({validAlertShow: false})
-        this.setState({invalidAlertShow: true})
-        }
-    }
-    
-    handleSettingsInput = (e, methodParameter, id) => {
-      e.preventDefault();
-
-      let dataDevice = {
-        "groupId": 1,
-        "name" : e.target.elements.formNameDevice.value.trim(),
-        "modele": 1,
-        "type": 1,
-        "ip": e.target.elements.formIpAddress.value.trim(),
-        }
-      let regexIpv4Address = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
-      let valueIpv4AddressForm = dataDevice.ip;
-
-      if(((e.target.elements.formNameDevice.value).trim() == "") && !(regexIpv4Address.test(valueIpv4AddressForm))) {
-        this.handleInvalidAlertShow();
-        this.state.messageErreur = "le nom de l'appareil doit contenir au moins un caractère ou le format de l'adresse IP n'est pas valide.";
-        }
-      else if ((e.target.elements.formNameDevice.value).trim() == "") {
-        this.handleInvalidAlertShow();
-        this.state.messageErreur = "le nom de l'appareil doit contenir au moins un caractère.";
-        } 
-      else if ((valueIpv4AddressForm).length > 15 || !(regexIpv4Address.test(valueIpv4AddressForm))) {
-        this.handleInvalidAlertShow();
-        this.state.messageErreur = "le format de l'adresse IP n'est pas valide.";
-        }
-
-        else {
-          fetch("https://localhost/api/devices" + (isNaN(id) ? "" : `/${id}`), {method: methodParameter,
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8'
-            },
-            body: JSON.stringify(dataDevice)
-         })
-             .then(response => response.json())
-             .then(this.props.displayDevice.bind(this))
-    }
+interface Device {
+  id: number
+  groupId: number
+  name: string
+  modele: string
+  type: number
+  ip: string
 }
-    
-    render (){
-      return (
-        <div>
-          <Button variant="secondary" onClick={this.handleShow.bind(this)} >
-            {this.props.buttonFeature}
-          </Button>
-            
-          <Modal show={this.state.modalShow} onHide={this.handleClose.bind(this)} animation={true}>
-            <Form onSubmit={(e) => this.handleSettingsInput(e, this.props.fetchMethod, this.props.id)}>
-              <Modal.Header closeButton>
-                <Modal.Title>Paramètres de l'appareil</Modal.Title>
-              </Modal.Header>
 
-              <Modal.Body> 
-                <FormDeviceOptions name={this.props.name} ipAddress={this.props.ipAddress} id={this.props.id} groupId={this.props.groupId} type={this.props.type} displayDevice={this.props.displayDevice} />
-                <Alert variant="success" show={this.state.validAlertShow}>
-                  {((this.props.fetchMethod == "PUT")  ? `La configuration de l'appareil ${this.props.name} a  bien été sauvegardée.` : `L'appareil a bien été rajouté à la liste des appareils.` )}
-                </Alert>
-                <Alert variant="danger" show={this.state.invalidAlertShow}>
-                  La configuration de l'appareil « {this.props.name} » ne peut pas être sauvegardée car {this.state.messageErreur}
-                </Alert>
-              </Modal.Body>
-                    
-              <Modal.Footer>
-                <Button variant="secondary" onClick={this.handleClose.bind(this)}>
-                  Fermer la fenêtre
-                </Button>
-                <Button variant="primary" onClick={this.handleValidAlertShow.bind(this)} type="submit">
-                  Sauvegarder
-                </Button>
-              </Modal.Footer>
-            </Form>
-          </Modal>
-        </div>
-        );
+interface DevicesManagementInterface {
+  modalShow: boolean;
+  setModalShow: boolean;
+  validAlertShow: boolean;
+  invalidAlertShow: boolean;
+  setInvalidAlertShow: boolean;
+  setValidAlertShow: boolean;
+  modalErrorMessage: string,
+}
+
+class DevicesManagement extends React.Component {
+
+  state: TypeState = {
+    modalShow: false,
+    setModalShow: false,
+    validAlertShow: false,
+    invalidAlertShow: false,
+    setInvalidAlertShow: false,
+    setValidAlertShow: false,
+    modalErrorMessage: "",
+  }
+
+
+  handleClose() {
+    this.setState({ modalShow: false });
+    this.setState({ validAlertShow: false });
+  }
+  handleShow() {
+    this.setState({ modalShow: true });
+  }
+  handleValidAlertShow() {
+    if (this.invalidAlertShow) {
+      this.setState({ validAlertShow: false });
+    } else {
+      this.setState({ validAlertShow: true });
+      this.setState({ invalidAlertShow: false });
+    }
+  }
+
+  handleInvalidAlertShow() {
+    if (this.validAlertShow) {
+      this.setState({ invalidAlertShow: false })
+    } else {
+      this.setState({ validAlertShow: false })
+      this.setState({ invalidAlertShow: true })
+    }
+  }
+
+  handleSettingsInput = (e, methodParameter, id) => {
+    e.preventDefault();
+
+    let dataDevice = {
+      "groupId": 1,
+      "name": e.target.elements.formNameDevice.value.trim(),
+      "modele": 1,
+      "type": 1,
+      "ip": e.target.elements.formIpAddress.value.trim(),
+    }
+    let regexIpv4Address = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+    let valueIpv4AddressForm = dataDevice.ip;
+
+    if (((e.target.elements.formNameDevice.value).trim() == "") && !(regexIpv4Address.test(valueIpv4AddressForm))) {
+      this.handleInvalidAlertShow();
+      this.state.messageErreur = "le nom de l'appareil doit contenir au moins un caractère ou le format de l'adresse IP n'est pas valide.";
+    }
+    else if ((e.target.elements.formNameDevice.value).trim() == "") {
+      this.handleInvalidAlertShow();
+      this.state.messageErreur = "le nom de l'appareil doit contenir au moins un caractère.";
+    }
+    else if ((valueIpv4AddressForm).length > 15 || !(regexIpv4Address.test(valueIpv4AddressForm))) {
+      this.handleInvalidAlertShow();
+      this.state.messageErreur = "le format de l'adresse IP n'est pas valide.";
+    }
+
+    else {
+      fetch("/api/devices" + (isNaN(id) ? "" : `/${id}`), {
+        method: methodParameter,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify(dataDevice)
+      })
+        .then(response => response.json())
+        .then(this.props.displayDevice.bind(this))
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Button variant="secondary" onClick={this.handleShow.bind(this)} >
+          {this.props.buttonFeature}
+        </Button>
+
+        <Modal show={this.state.modalShow} onHide={this.handleClose.bind(this)} animation={true}>
+          <Form onSubmit={(e) => this.handleSettingsInput(e, this.props.fetchMethod, this.props.id)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Paramètres de l'appareil</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <FormDeviceOptions name={this.props.name} ipAddress={this.props.ipAddress} id={this.props.id} groupId={this.props.groupId} type={this.props.type} displayDevice={this.props.displayDevice} />
+              <Alert variant="success" show={this.state.validAlertShow}>
+                {((this.props.fetchMethod == "PUT") ? `La configuration de l'appareil ${this.props.name} a  bien été sauvegardée.` : `L'appareil a bien été rajouté à la liste des appareils.`)}
+              </Alert>
+              <Alert variant="danger" show={this.state.invalidAlertShow}>
+                La configuration de l'appareil « {this.props.name} » ne peut pas être sauvegardée car {this.state.messageErreur}
+              </Alert>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose.bind(this)}>
+                Fermer la fenêtre
+              </Button>
+              <Button variant="primary" onClick={this.handleValidAlertShow.bind(this)} type="submit">
+                Sauvegarder
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+      </div>
+    );
   };
 }
 
--- web/src/pages/Details.tsx
+++ web/src/pages/Details.tsx
@@ -1,92 +1,89 @@
 import React from 'react'
-import {MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge} from "mdbreact";
+import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge } from "mdbreact";
 import GraphiqueEnergie from "../components/GraphiqueEnergie";
-//import { Form } from 'react-bootstrap'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 class Details extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            devices: []
-        };
-    }
-
-    componentDidMount(){
-        let id = this.props.match.params.id
-		this.detailsApi(id);
-	}
-
-    detailsApi(idParameter) {
-
-        fetch(`https://localhost/api/devices/` + idParameter)
-            .then(res => res.json())
-            .then((result) => {
-                    this.setState({
-                        isLoaded: true,
-                        devices: result
-                    })
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
-
-
-    render() {
-        const {error, isLoaded, devices} = this.state;
-        console.log(devices);
-        console.log(typeof (id));
-
-        if (error) {
-            return <div>Erreur: {error.message}</div>
-        } else if (!isLoaded) {
-            return <div>Chargement...</div>;
-        } else {
-            return (
-                <div>
-
-                    <MDBContainer>
-                        <MDBListGroup style={{width: "22rem"}}>
-                            <MDBListGroupItem
-                                className="d-flex justify-content-between align-items-center">Name: {devices.name}<MDBBadge
-                                color="primary"
-                                pill></MDBBadge>
-                            </MDBListGroupItem>
-                            <MDBListGroupItem
-                                className="d-flex justify-content-between align-items-center">Groupe: {devices.groupId}<MDBBadge
-                                color="primary"
-                                pill></MDBBadge>
-                            </MDBListGroupItem>
-                            <MDBListGroupItem
-                                className="d-flex justify-content-between align-items-center">Modèle: {devices.modele}<MDBBadge
-                                color="primary" pill></MDBBadge>
-                            </MDBListGroupItem>
-                            <MDBListGroupItem
-                                className="d-flex justify-content-between align-items-center">IP: {devices.ip}<MDBBadge
-                                color="primary"
-                                pill></MDBBadge>
-                            </MDBListGroupItem>
-                            <MDBListGroupItem
-                                className="d-flex justify-content-between align-items-center">Type: {devices.type}<MDBBadge
-                                color="primary"
-                                pill></MDBBadge>
-                            </MDBListGroupItem>
-                        </MDBListGroup>
-                        <GraphiqueEnergie data={[15, 24, 18, 19]} labels={["test1", "test2", "test3", "test4"]}/>
-                    </MDBContainer>
-
-                </div>
-            )
-        }
-        ;
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      devices: []
     };
+  }
+
+  componentDidMount() {
+    let id = this.props.match.params.id
+    this.detailsApi(id);
+  }
+
+  detailsApi(idParameter) {
+
+    fetch(`/api/devices/` + idParameter)
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          isLoaded: true,
+          devices: result
+        })
+      },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+  render() {
+    const { error, isLoaded, devices } = this.state;
+
+    if (error) {
+      return <div>Erreur: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Chargement...</div>;
+    } else {
+      return (
+        <div>
+
+          <MDBContainer>
+            <MDBListGroup style={{ width: "22rem" }}>
+              <MDBListGroupItem
+                className="d-flex justify-content-between align-items-center">Name: {devices.name}<MDBBadge
+                  color="primary"
+                  pill></MDBBadge>
+              </MDBListGroupItem>
+              <MDBListGroupItem
+                className="d-flex justify-content-between align-items-center">Groupe: {devices.groupId}<MDBBadge
+                  color="primary"
+                  pill></MDBBadge>
+              </MDBListGroupItem>
+              <MDBListGroupItem
+                className="d-flex justify-content-between align-items-center">Modèle: {devices.modele}<MDBBadge
+                  color="primary" pill></MDBBadge>
+              </MDBListGroupItem>
+              <MDBListGroupItem
+                className="d-flex justify-content-between align-items-center">IP: {devices.ip}<MDBBadge
+                  color="primary"
+                  pill></MDBBadge>
+              </MDBListGroupItem>
+              <MDBListGroupItem
+                className="d-flex justify-content-between align-items-center">Type: {devices.type}<MDBBadge
+                  color="primary"
+                  pill></MDBBadge>
+              </MDBListGroupItem>
+            </MDBListGroup>
+            <GraphiqueEnergie data={[15, 24, 18, 19]} labels={["test1", "test2", "test3", "test4"]} />
+          </MDBContainer>
+
+        </div>
+      )
+    }
+    ;
+  };
 }
 
 export default Details;
