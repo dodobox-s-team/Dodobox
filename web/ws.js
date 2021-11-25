2a0aaea051267507899858f4ba83105be0c6b31c
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-11-25 15:15:47 +0100

    ADD offline.html

--- web/ws.js
+++ web/ws.js
@@ -1,18 +1,37 @@
+const PREFIX = 'V2';
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(PREFIX);
+      console.log("cache")
+      cache.add(new Request('/offline.html'));
+    })()
+  );
+  console.log(`${PREFIX} Install`);
+});
+
+self.addEventListener("activate", () => {
+  clients.claim()
+  console.log(`${PREFIX} Active`);
+});
+
 self.addEventListener("fetch", (event) => {
-    console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`)
-    if (event.request.mode == 'navigate') {
-        event.respondWith(
-		(async () => {
-			try {
-			const preloadResponse = await event.preloadResponse;
-			if (preloadResponse) {
-				return preloadResponse;
-			}
-			
-			return await fetch(event.request);
-			} catch (e) {
-				return new Response("Bonjour les gens");
-			}
-        })());
-    }
+  if (event.request.mode == 'navigate') {
+    console.log(`${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          return await fetch(event.request);
+        } catch (e) {
+          const cache = await caches.open(PREFIX);
+          return await cache.match('/offline.html');
+        }
+      })());
+  }
 });
