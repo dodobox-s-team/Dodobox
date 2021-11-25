86c53f7c466311e13300f2b6f51189d24e4a067e
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-11-25 11:57:57 +0100

    ADD serviceWorker

---
+++ web/ws.js
@@ -0,0 +1,8 @@
+self.addEventListener("fetch", (event) => {
+    console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`)
+    if (event.request.mode == 'navigate') {
+        event.respondWith((async () => {
+            return new Response("Bonjour les gens");
+        })());
+    }
+});
