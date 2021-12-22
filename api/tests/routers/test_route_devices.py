dc798afbd38dbb804dcf6a49029706f52e67377f
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2021-12-22 17:28:37 +0100

    [modif] test des routes des devices
    test coverage de 93%

--- api/tests/routers/test_route_devices.py
+++ api/tests/routers/test_route_devices.py
@@ -16,30 +16,36 @@ class TestRouteDevices:
         # Add a device
         response = await client.post("/devices", json=device2.dict())
         assert response.status_code == status.HTTP_200_OK
-        assert response.json() == device2
+        assert response.json() == device2.dict()
 
         # Check if the device we added has been added.
         # We run the tests against a fresh database every time, so it should contain only our device.
         response = await client.get(f"/devices/{device2.id}")
         assert response.status_code == status.HTTP_200_OK
-        assert response.json() == device2
+        assert response.json() == device2.dict()
 
     async def test_get_device(self, client: TestClient, device: Device):
         # Also check that the path to the individual resource works
         response = await client.get(f"/devices/{device.id}")
         assert response.status_code == status.HTTP_200_OK
-        assert response.json() == device
+        assert response.json() == device.dict()
 
         # Get a nonexistant resource should return 404
-        response = await client.get("/devices/404")
+        response = await client.get("/devices/666")
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    async def test_get_devices_status(self, client: TestClient, device: Device):
+        # Check that the device is True or False
+        response = await client.get(f"/devices/{device.id}/status")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {'toggle': device.toggle}
+
+        # Get a nonexistant resource should return 404
+        response = await client.get("/devices/404/status")
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
     async def test_edit_device(self, client: TestClient, device: Device):
         device.name = new_name = "Ampoule"
-
-        # Update a device that doesn't exist. It should return a 404 error.
-        response = await client.put("/devices/666", json=device.dict())
-        assert response.status_code == status.HTTP_404_NOT_FOUND
 
         # Update the device's name.
         response = await client.put(f"/devices/{device.id}", json=device.dict())
@@ -51,6 +57,27 @@ class TestRouteDevices:
         assert response.status_code == status.HTTP_200_OK
         assert response.json().get("name") == new_name
 
+        # Update a device that doesn't exist. It should return a 404 error.
+        response = await client.put("/devices/666", json=device.dict())
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    async def test_edit_device_status(self, client: TestClient, device: Device):
+        device.toggle = new_toggle = False
+
+        # Update the device's status.
+        response = await client.put(f"/devices/{device.id}/{device.toggle}", json=device.dict())
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json().get("toggle") == device.toggle
+
+        # Check the status has changed
+        response = await client.put(f"/devices/{device.id}/{device.toggle}", json=device.dict())
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json().get("toggle") == new_toggle
+
+        # Update a device that doesn't exist. It should return a 404 error.
+        response = await client.put("/devices/666", json=device.dict())
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
     async def test_delete_device(self, client: TestClient, device: Device):
         # Check for a device that doesn't exists
         response = await client.delete("/devices/666")
@@ -60,9 +87,3 @@ class TestRouteDevices:
         response = await client.delete(f"/devices/{device.id}")
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == device
-
-        # Check that the device does not exists anymore.
-        #response = await client.get("/devices")
-        #assert response.status_code == status.HTTP_200_OK
-        #assert response.json() == []
-
