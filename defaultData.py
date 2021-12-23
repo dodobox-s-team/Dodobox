4c37ad524366b010e062c171fd987cfe7b0f24a9
QuentinServais <64021670+QuentinServais@users.noreply.github.com>
2021-12-23 06:03:21 +0100

    [defaultData] update variable domain name

--- defaultData.py
+++ defaultData.py
@@ -2,27 +2,27 @@
 import requests
 import json
 
+name = 'quentin'
+
 # Create groups
-requests.post('https://severin.dodobox.site/api/groups', data=json.dumps({"id": 0,"name": "Salon"}))
-requests.post('https://severin.dodobox.site/api/groups', data=json.dumps({"id": 0,"name": "Cuisine"}))
-requests.post('https://severin.dodobox.site/api/groups', data=json.dumps({"id": 0,"name": "Salle de bain"}))
+requests.post(f'https://{name}.dodobox.site/api/groups', data=json.dumps({"id": 0,"name": "Salon"}))
+requests.post(f'https://{name}.dodobox.site/api/groups', data=json.dumps({"id": 0,"name": "Cuisine"}))
+requests.post(f'https://{name}.dodobox.site/api/groups', data=json.dumps({"id": 0,"name": "Salle de bain"}))
 
 # Create modules for each group
-requests.post('https://severin.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 1,"name": "thermomètre","modele": "string","type": 1,"ip": "192.168.1.99","toggle": True}))
-requests.post('https://severin.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 1,"name": "humidité","modele": "string","type": 2,"ip": "192.168.1.99","toggle": True}))
-requests.post('https://severin.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 1,"name": "qualité de l\'air","modele": "string","type": 3,"ip": "192.168.1.99","toggle": True}))
+requests.post(f'https://{name}.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 1,"name": "thermomètre","modele": "string","type": 1,"ip": "192.168.1.99","toggle": True}))
+requests.post(f'https://{name}.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 1,"name": "humidité","modele": "string","type": 2,"ip": "192.168.1.99","toggle": True}))
+requests.post(f'https://{name}.dodobox.site/api//devices', data=json.dumps({"id": 0,"groupId": 1,"name": "qualité de l\'air","modele": "string","type": 3,"ip": "192.168.1.99","toggle": True}))
 
-requests.post('https://severin.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 2,"name": "qualité de l\'air","modele": "string","type": 3,"ip": "192.168.1.95","toggle": True}))
+requests.post(f'https://{name}.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 2,"name": "qualité de l\'air","modele": "string","type": 3,"ip": "192.168.1.95","toggle": True}))
 
-requests.post('https://severin.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 3,"name": "thermomètre","modele": "string","type": 1,"ip": "192.168.1.124","toggle": True}))
-requests.post('https://severin.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 3,"name": "humidité","modele": "string","type": 2,"ip": "192.168.1.124","toggle": True}))
+requests.post(f'https://{name}.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 3,"name": "thermomètre","modele": "string","type": 1,"ip": "192.168.1.124","toggle": True}))
+requests.post(f'https://{name}.dodobox.site/api/devices', data=json.dumps({"id": 0,"groupId": 3,"name": "humidité","modele": "string","type": 2,"ip": "192.168.1.124","toggle": True}))
 
 # Create graph
-requests.post('https://severin.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 1,"name": "thermoSalon","axisLabel": "°C"}))
-requests.post('https://severin.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 2,"name": "humiSalon","axisLabel": "°C"}))
-requests.post('https://severin.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 3,"name": "qualiSalon","axisLabel": "q"}))
-requests.post('https://severin.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 4,"name": "qualiCuisine","axisLabel": "q"}))
-requests.post('https://severin.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 5,"name": "thermoSDB","axisLabel": "°C"}))
-requests.post('https://severin.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 6,"name": "humiSDB","axisLabel": "%"}))
-
-
+requests.post(f'https://{name}.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 1,"name": "thermoSalon","axisLabel": "°C"}))
+requests.post(f'https://{name}.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 2,"name": "humiSalon","axisLabel": "°C"}))
+requests.post(f'https://{name}.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 3,"name": "qualiSalon","axisLabel": "q"}))
+requests.post(f'https://{name}.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 4,"name": "qualiCuisine","axisLabel": "q"}))
+requests.post(f'https://{name}.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 5,"name": "thermoSDB","axisLabel": "°C"}))
+requests.post(f'https://{name}.dodobox.site/api/graph', data=json.dumps({"id": 0,"deviceId": 6,"name": "humiSDB","axisLabel": "%"}))
